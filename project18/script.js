32d3765a78e37e270dd1162ac50ba96d80d436c8
akzain <zainkamaal66885@gmail.com>
2021-04-07 06:37:24 +0300

    project 18 js updated to workd

--- project18/script.js
+++ project18/script.js
@@ -3,21 +3,30 @@ const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
+let activeSlide = 0
+
 rightBtn.addEventListener('click', () => {
     activeSlide++
-    if (activeSlide > slides.length - 1) { activeSlide = 0 }
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
     setBgToBody()
     setActiveSlide()
 })
 
 leftBtn.addEventListener('click', () => {
     activeSlide--
-    if (activeSlide < 0) { activeSlide = slide.length - 1 }
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+
     setBgToBody()
     setActiveSlide()
 })
 
-let activeSlide = 0
 setBgToBody()
 
 function setBgToBody() {
@@ -25,7 +34,7 @@ function setBgToBody() {
 }
 
 function setActiveSlide() {
-    slides.forEach(slide => slide.classList.remove('active'))
+    slides.forEach((slide) => slide.classList.remove('active'))
 
     slides[activeSlide].classList.add('active')
 }
